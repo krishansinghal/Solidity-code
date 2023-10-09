4c340472533f0bb5ba97dde0c2674ac80e645457
krishansinghal <141732909+krishansinghal@users.noreply.github.com>
2023-10-09 12:07:07 +0530

    Update even-number.sol

--- even-number.sol
+++ even-number.sol
@@ -1,9 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0
 
+//solidity version.
 pragma solidity >=0.7.0 <0.9.0;
 
-contract MyContract {
+//creating contract named A.
+contract A {
+//Declaring int variable named num.
     int public num;
+//creating function to check even number.
     function Checkeven(int _num) public  pure returns(bool){
         if(_num%2==0){
             return true;
